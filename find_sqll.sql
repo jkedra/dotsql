3cf1f83de1cecc0d17fe792a7b6091cdeebeb63e
Jurek Kedra <jurek.kedra@gmail.com>
2013-11-20 13:13:58 +0100

    11g changes, sql_fulltext instead of sql_text

--- find_sqll.sql
+++ find_sqll.sql
@@ -1,4 +1,6 @@
-set long 4000
+-- vim:ts=4
+-- 11g up version
+--
 set verify off
 set pagesize 999
 col username format a13
@@ -10,13 +12,14 @@ col ocategory format a10
 col avg_etime format 9,999,999.99
 col etime format 9,999,999.99
 
-select sql_id, child_number, plan_hash_value plan_hash, executions execs, elapsed_time/1000000 etime,
-(elapsed_time/1000000)/decode(nvl(executions,0),0,1,executions) avg_etime, u.username,
-sql_fulltext
-from v$sql s, dba_users u
-where upper(sql_text) like upper(nvl('%&sql_text%',sql_text))
-and sql_text not like '%from v$sql where sql_text like nvl(%'
-and sql_id like nvl('&sql_id',sql_id)
-and u.user_id = s.parsing_user_id
+SELECT /* sq9mcp */ sql_id, child_number, plan_hash_value plan_hash,
+	executions execs, elapsed_time/1000000 etime,
+	(elapsed_time/1000000)/decode(nvl(executions,0),0,1,executions) avg_etime,
+	u.username, sql_fulltext
+FROM v$sql s, dba_users u
+WHERE UPPER(sql_fulltext) LIKE UPPER(NVL('%&sql_text%',sql_fulltext))
+AND sql_text NOT LIKE '%from v$sql where sql_text like nvl(%'
+AND sql_id LIKE nvl('&sql_id',sql_id)
+AND u.user_id = s.parsing_user_id
 /
 
--- gfind_sqll.sql
+++ gfind_sqll.sql
@@ -1,7 +1,11 @@
 -- vim:ts=4
 -- find_sql version for all instances (g version)
+-- this version works with >v11, previously it used
+-- sql_text instead sql_fulltext to comparision
+-- because sql_fulltext was LONG (now CLOB) and LIKE didn't work
 set verify off
-set pagesize 999
+SET pagesize 999
+SET LINESIZE 160
 SET NUMWIDTH 3
 col username format a13
 col prog format a22
@@ -14,13 +18,18 @@ col etime format 9,999,999.99
 
 COL sqlid HEADING "sql_id|plan_hash" FORMAT A13
 
-SELECT sql_id||'  '||plan_hash_value sqlid, child_number, inst_id ins,
+SELECT /* sq9mcp */ sql_id||'  '||plan_hash_value sqlid, child_number, inst_id ins,
 	executions excs, elapsed_time/1000000 etime,
 	(elapsed_time/1000000)/decode(nvl(executions,0),0,1,executions) avg_etime,
 	u.username, sql_fulltext
 FROM gv$sql s, dba_users u
-WHERE upper(sql_text) LIKE upper(nvl(q'{%&sql_text%}',sql_text))
-	AND  sql_text NOT LIKE '%from gv$sql where sql_text like nvl(%'
+WHERE upper(sql_fulltext) LIKE upper(nvl(q'{%&sql_text%}',sql_fulltext))
+	AND NOT
+	(
+		sql_text LIKE '%sq9mcp%' OR
+		sql_text LIKE 'EXPLAIN PLAN %' OR
+		sql_text LIKE '%v$sql s, dba_users u%'
+	)
 	AND sql_id LIKE nvl('&sql_id',sql_id)
 	AND u.user_id = s.parsing_user_id
 /
