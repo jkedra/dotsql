a232f75edefbcc91946698ef85d4a51c4e919f24
Jurek Kedra <jurek.kedra@gmail.com>
2013-11-20 13:13:58 +0100

    11g changes, sql_fulltext instead of sql_text

--- gfind_sqll.sql
+++ gfind_sqll.sql
@@ -1,5 +1,8 @@
 -- vim:ts=4
 -- find_sql version for all instances (g version)
+-- this version works with >v11, previously it used
+-- sql_text instead sql_fulltext to comparision
+-- because sql_fulltext was LONG (now CLOB) and LIKE didn't work
 set verify off
 set pagesize 999
 SET NUMWIDTH 3
@@ -14,13 +17,13 @@ col etime format 9,999,999.99
 
 COL sqlid HEADING "sql_id|plan_hash" FORMAT A13
 
-SELECT sql_id||'  '||plan_hash_value sqlid, child_number, inst_id ins,
+SELECT /* sq9mcp */ sql_id||'  '||plan_hash_value sqlid, child_number, inst_id ins,
 	executions excs, elapsed_time/1000000 etime,
 	(elapsed_time/1000000)/decode(nvl(executions,0),0,1,executions) avg_etime,
 	u.username, sql_fulltext
 FROM gv$sql s, dba_users u
-WHERE upper(sql_text) LIKE upper(nvl(q'{%&sql_text%}',sql_text))
-	AND  sql_text NOT LIKE '%from gv$sql where sql_text like nvl(%'
+WHERE upper(sql_fulltext) LIKE upper(nvl(q'{%&sql_text%}',sql_fulltext))
+	AND  sql_text NOT LIKE '% sq9mcp %'
 	AND sql_id LIKE nvl('&sql_id',sql_id)
 	AND u.user_id = s.parsing_user_id
 /
