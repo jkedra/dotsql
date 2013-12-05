0945fe1507efe4c3475aa114ebf2ddf574ec86cc
Jurek Kedra <jurek.kedra@gmail.com>
2013-12-05 12:41:28 +0100

    added host info

--- uptime.sql
+++ uptime.sql
@@ -1,5 +1,7 @@
 
-select instance_name inst, to_char(startup_time,'DD.MM.YY HH24:MM:SS') time
+COL host_name FORMAT A20
+COL host      FORMAT A20
+select instance_name inst, host_name host, to_char(startup_time,'DD.MM.YY HH24:MM:SS') time
 from gv$instance;
 
 
