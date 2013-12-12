966719908077e17d978f5a65cd543c370af7564c
Jurek Kedra <jurek.kedra@gmail.com>
2013-12-12 12:47:14 +0100

    Display longer version of the execution plan.

---
+++ dplanl.sql
@@ -0,0 +1,12 @@
+set verify off
+set pages 9999
+set lines 200
+PROMPT Additional flags
+PROMPT ALL ALLSTATS +peeked_bind
+PROMPT Spooling to /tmp/plan.txt
+
+SPOOL /tmp/plan.txt
+select * from table(dbms_xplan.display_cursor('&sql_id','&child_no','ALL ALLSTATS +peeked_binds -MEMSTATS'))
+/
+SPOOL OFF
+
