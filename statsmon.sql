227fdd498410bcb1c4c2fefe746b3e8d197fe7f9
Jurek Kedra <jerzy.kedra@sabre.com>
2014-02-20 14:40:16 +0100

    statsmon.sql: further format report improvements

--- statsmon.sql
+++ statsmon.sql
@@ -3,6 +3,9 @@
 -- statsmon.sql: monitoring concurrent statistics job
 
 SET lines 160
-COL comments FORMAT A30
+COL comments FORMAT A45
+COL state    FORMAT A10
+COL job_name FORMAT A15
+
 SELECT job_name, state, comments FROM dba_scheduler_jobs
 	WHERE job_class LIKE 'CONC%';
