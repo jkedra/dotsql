c87ad638f3382e2515bd9dffcb20c63daea5d0e2
Jurek Kedra <jerzy.kedra@sabre.com>
2014-03-12 13:02:04 +0100

    topavif.sql - top 10 largest tasdba tables

---
+++ topavif.sql
@@ -0,0 +1,17 @@
+SELECT rownum,table_name,total_size_MB FROM (
+SELECT table_name, (    SELECT ROUND(SUM(NVL(bytes,0))/1024/1024) "total MB"
+                        FROM dba_segments
+                        WHERE (owner,segment_name) IN (
+                            SELECT dt.owner, dt.table_name FROM dual
+                            UNION
+                            SELECT owner,segment_name FROM dba_lobs
+                            WHERE owner=dt.owner and table_name=dt.table_name
+                            UNION
+                            SELECT owner,index_name FROM dba_indexes
+                            WHERE owner=dt.owner and table_name=dt.table_name
+                        )
+                  ) total_size_MB
+FROM dba_tables dt WHERE owner='TASDBA' AND temporary='N'
+ORDER BY 2 DESC
+) WHERE ROWNUM<11;
+
