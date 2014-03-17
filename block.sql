e3bc77d134c81141958fc4d00bd730196b917009
Jurek Kedra <jerzy.kedra@sabre.com>
2014-03-17 12:45:41 +0100

    block.sql gv$session

--- block.sql
+++ block.sql
@@ -1,10 +1,11 @@
---
+-- LOCK
 -- process 1234 always for JDBC
 --
 SET LINES 160
 COL program FORMAT A30
+COL sidser# FORMAT A10
 select username,sid||'.'||serial# sidser#,
 	BLOCKING_SESSION_STATUS,BLOCKING_INSTANCE,FINAL_BLOCKING_SESSION,
 	substr(program,1,30) program,process
-	from v$session
+	from gv$session
 	WHERE blocking_session_status='VALID';
