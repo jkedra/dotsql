59ee2714dd735ba2ef73704b1a60a3edaa32cc72
Jurek Kedra <jerzy.kedra@sabre.com>
2014-03-31 10:51:01 +0200

    asmdisk: label added, format change

--- asmdisk.sql
+++ asmdisk.sql
@@ -1,8 +1,10 @@
-col path format a40
+col path format a25
+col name FORMAT A10
+col label FORMAT A10
 col dskgrp FORMAT A10
-set lines 160
+set lines 100
 
-select d.name,d.header_status,d.state,dg.name dskgrp, d.path
+SELECT d.name,d.label, d.header_status,d.state,dg.name dskgrp, d.path
 FROM v$asm_disk d, v$asm_diskgroup dg
-WHERE d.group_number=dg.group_number (+)
+WHERE d.group_number = dg.group_number (+)
 /
