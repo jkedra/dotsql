160d5c81f448fb28b3a2fbc1701775f669d85b68
Jurek Kedra <jerzy.kedra@sabre.com>
2014-06-17 10:56:59 +0200

    added ismodified<>'FALSE'

--- paramdump.sql
+++ paramdump.sql
@@ -8,7 +8,7 @@ SET LINES 120
 
 SELECT name,display_value
 FROM v$parameter
-WHERE isdefault='FALSE'
+WHERE (isdefault='FALSE' OR ismodified<>'FALSE')
 	AND name NOT IN ('db_name','control_files','spfile','audit_file_dest',
 	'log_archive_format')
 ORDER BY name,num;
