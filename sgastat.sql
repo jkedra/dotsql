9d311237e61dce5e4f2ae0d5c85759deba9b62c9
Jurek Kedra <jerzy.kedra@sabre.com>
2014-06-18 15:50:01 +0200

    sgastat - only top 20 rows is displayed now

--- sgastat.sql
+++ sgastat.sql
@@ -1,1 +1,13 @@
-select POOL,NAME,round(BYTES/1024/1024) MB   from v$sgastat;
+REM vim:ts=4:expandtab
+REM SELECT pool, name, ROUND(bytes/1024/1024) mb FROM v$sgastat ORDER BY pool, mb ASC;
+
+SELECT pool, name, mb
+FROM (
+    SELECT pool, name, mb,  ROW_NUMBER() OVER (PARTITION BY pool ORDER BY mb DESC) rn
+    FROM
+        (SELECT pool,name,ROUND(bytes/1024/1024) MB
+	     FROM v$sgastat
+        )
+)
+WHERE rn < 20
+ORDER BY pool, rn DESC;
