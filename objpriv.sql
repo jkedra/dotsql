e526d4d2166ac7d6b49f9491f32752012f43ecc2
Jurek Kedra <jerzy.kedra@sabre.com>
2014-06-25 10:51:32 +0200

    objpriv improvement

--- objpriv.sql
+++ objpriv.sql
@@ -3,6 +3,6 @@ COL grantee    FORMAT A20
 COL table_name FORMAT A30
 
 SELECT * FROM dba_tab_privs
-WHERE grantee='TASGEN' and owner='TASDBA'
+WHERE grantee='&grantee' and owner='&owner'
 ORDER BY owner,table_name,privilege;
 
