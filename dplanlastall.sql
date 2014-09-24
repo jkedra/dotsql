ade810dbfc5ae6c6e99d6c504fd4e7e2ebb5a68f
Jurek Kedra <jerzy.kedra@sabre.com>
2014-09-24 17:00:11 +0200

    dplanlastall: added info about SERVEROUTPUT

--- dplanlastall.sql
+++ dplanlastall.sql
@@ -1,6 +1,11 @@
 set verify off
 set pages 9999
 set lines 200
+PROMPT You have to issue SET SERVEROUTPUT OFF prior runing the query.
+PROMPT Otherwise the last statement you will have run will be
+PROMPT the (hidden) call to dbms_output that follows your execution
+PROMPT of any other statement – so you won’t get the plan and statistics.
+PROMPT
 PROMPT Additional flags
 PROMPT ALL ALLSTATS LAST
 
