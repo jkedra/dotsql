5fa95f412a8f0e6ab5cfce51ec1e331b55cc07e6
Jurek Kedra <jerzy.kedra@sabre.com>
2014-10-01 16:08:04 +0200

    topopencursors and inst

---
+++ topopencursors.sql
@@ -0,0 +1,11 @@
+
+SELECT * FROM (
+SELECT oc.inst_id,oc.sid||','||s.serial# sidser ,count(*) from gv$open_cursor oc, gv$session s
+WHERE oc.inst_id=s.inst_id AND oc.saddr=s.saddr
+GROUP BY oc.inst_id,oc.sid||','||s.serial#
+ORDER BY count(*) DESC
+)
+WHERE rownum <= 10
+/
+
+
