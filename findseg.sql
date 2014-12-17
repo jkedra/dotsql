a66a0922e460c2dbd27cf8a51863b76276a87dd6
Jurek Kedra <jerzy.kedra@sabre.com>
2014-12-17 14:30:59 +0100

    variuos new scripts

---
+++ findseg.sql
@@ -0,0 +1,16 @@
+ACCEPT vtabsp CHAR  PROMPT  "tablespace name (% allowed):"
+
+COL owner FORMAT A15
+COL segment_name FORMAT A30
+COL segment_type FORMAT A12
+
+SET LINES 120
+
+SELECT * FROM (
+  SELECT owner,segment_type,segment_name,ROUND(SUM(bytes)/1024/1024,1) MB
+    FROM dba_segments
+    WHERE tablespace_name LIKE '&vtabsp'
+    GROUP BY owner,segment_type,segment_name
+    ORDER BY 3 DESC
+) WHERE rownum <11;
+
