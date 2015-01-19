8b2a74ed3e7622ae1ccaa969950388dba22a2763
Jurek Kedra <jerzy.kedra@sabre.com>
2015-01-19 10:11:41 +0100

    seqraise-tasgen improvements
    unmasking exceptions, adding owner prefix

--- seqraise-tasgen.sql
+++ seqraise-tasgen.sql
@@ -1,10 +1,18 @@
+-- vim:ts=4:expandtab
 DECLARE
+tableview_notexist EXCEPTION;
+PRAGMA exception_init(tableview_notexist, -942);
 table_pattern VARCHAR2(128);
 vsqlmaxtbl VARCHAR2(256);
 vsqlseq VARCHAR2(256);
 cnttbl INTEGER;
 cntseq INTEGER;
 vschema_owner VARCHAR2(30);
+--
+-- all_tables ->
+--       all_sequeces with names like the table one ->
+--          all_tab_columns like "ID"
+--
 BEGIN
     SELECT value INTO vschema_owner FROM adm_setup WHERE property='SCHEMA OWNER';
     
@@ -17,13 +25,13 @@ BEGIN
                  )
         LOOP
             cntseq := s.last_number;
-            -- just assume id is always related with SEQ
+            -- just assume column ID is always related with SEQ
             FOR i IN (SELECT * FROM all_tab_columns
                          WHERE owner=vschema_owner
                            AND table_name=t.table_name AND column_name='ID')
             LOOP
             BEGIN
-                vsqlmaxtbl := 'SELECT MAX('||i.column_name||') FROM '||t.table_name;
+                vsqlmaxtbl := 'SELECT MAX('||i.column_name||') FROM '||t.owner||'.'||t.table_name;
                 --DBMS_OUTPUT.PUT_LINE(vsqlmaxtbl);
                 EXECUTE IMMEDIATE vsqlmaxtbl INTO cnttbl;
                 IF cnttbl IS NULL THEN
@@ -32,15 +40,15 @@ BEGIN
                 DBMS_OUTPUT.PUT_LINE(t.table_name||'('||cnttbl||'): '||s.sequence_name||'('||cntseq||')' );
                 IF cnttbl > cntseq THEN
                 LOOP
-                     vsqlseq := 'SELECT '||s.sequence_name||'.NEXTVAL FROM dual';
+                     vsqlseq := 'SELECT '||s.sequence_owner||'.'||s.sequence_name||'.NEXTVAL FROM dual';
                      EXECUTE IMMEDIATE vsqlseq INTO cntseq;
                      EXIT WHEN cntseq >= cnttbl;
                 END LOOP;
                 END IF;
                 DBMS_OUTPUT.PUT_LINE(t.table_name||'('||cnttbl||'): '||s.sequence_name||'('||cntseq||')' );
             EXCEPTION
-                WHEN OTHERS THEN
-                    NULL;
+                WHEN tableview_notexist THEN
+                    DBMS_OUTPUT.PUT_LINE('[INFO] Object not exists T:'||t.table_name||' SQ:'||s.sequence_name);
             END;
             END LOOP;
         END LOOP;
