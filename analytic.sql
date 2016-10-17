1f4b28d3454b8d4d76a13d5b348fb37893092a4a
Jurek Kedra <jurek.kedra@gmail.com>
2016-10-17 22:39:04 +0200

    Refactoring a* scripts

--- analytic.sql
+++ analytic.sql
@@ -1,5 +1,9 @@
 -- vim:ts=4
 -- http://asktom.oracle.com/pls/asktom/f?p=100:11:0::::P11_QUESTION_ID:47910227585839
+
+--
+-- below examples of usage
+--
 select min(err_ivc_seq_nr), max(err_ivc_seq_nr),
 	count(*), nt
 from ( select err_ivc_seq_nr, ntile(10)
@@ -14,19 +18,11 @@ SELECT min(brd_flt_dt), max(brd_flt_dt), count(*), nt
 FROM (
 	SELECT i.brd_flt_dt, ntile(10) 
         	OVER(ORDER BY brd_flt_dt) nt
-        FROM tasdba.tasr_ivc i, tasdba.tasr_apcppdf a
+        FROM tasr_ivc i, tasr_apcppdf a
         	WHERE i.ivc_seq_nr=a.ctr_ivc_seq_nr
 			AND .brd_flt_dt < SYSDATE-15
 	)
 GROUP BY NT
  ORDER BY NT;        
 
-/*
-
- DELETE tasdba.tasr_apcppdf A
-    WHERE EXISTS (SELECT ivc_seq_nr FROM tasdba.tasr_ivc i
-                  WHERE ivc_seq_nr=A.ctr_ivc_seq_nr
-                    AND brd_flt_dt < TO_DATE('&date', 'DD.MM.YY'));
-
-*/
 
--- asmgroup.sql
+++ asmgroup.sql
@@ -2,9 +2,10 @@ set lines 160
 col name format a15
 
 PROMPT T=TYPE (E=EXTERN)
-select group_number||':'||name name,
+SELECT group_number||':'||name name,
 	DECODE(type, 'EXTERN', 'E', type) T,
 	ROUND(total_mb/1024,2) GB,
 	ROUND(usable_file_mb/total_mb*100, 2) PCT_FREE,
-	ROUND(usable_file_mb/1024,2) USABLE from v$asm_diskgroup;
+	ROUND(usable_file_mb/1024,2) USABLE
+  FROM v$asm_diskgroup;
 
