9651bec670d97b3524a45ecd5786f5281e44f9fc
Kedra, Jurek <Jerzy.Kedra@sabre.com>
2018-02-21 18:18:34 +0100

    auditing scripts

---
+++ auditlogon.sql
@@ -0,0 +1,23 @@
+-- parameters: audit_trail=DB
+-- audit create session whenever not successful;
+COL os_username format A8
+col userhost format A10
+COL client_id FORMAT A20 WORD_WRAPPED
+COL error FORMAT A30
+SET LINES 160
+SET VERIFY OFF
+
+ACCEPT vhours NUMBER DEFAULT 1 PROMPT "hours back [1]: " 
+      
+SELECT username, userhost, client_id,
+    TO_CHAR(timestamp, 'DD.MM.YYYY HH24:MI') evtime,
+    DECODE(returncode, 1017, 'invalid username/password; logon denied',
+           returncode, 28000, 'account locked',
+           returncode) error
+FROM sys.dba_audit_session
+    WHERE action_name='LOGON'
+      AND (username  IN ('TASGEN', 'ASDM', 'AUTOUSER', 'ZABBIX')
+           OR REGEXP_LIKE(username, '..\(WEB|BAT\).$'))
+      AND returncode<>0
+      AND timestamp > SYSTIMESTAMP-&vhours./24
+      ORDER BY timestamp DESC;   
--- auditlogon12.sql
+++ auditlogon12.sql
@@ -1,4 +1,7 @@
+-- parameters: audit_trail=DB
+-- audit create session whenever not successful;
 COL os_username format A8
+COL dbusername FORMAT A8
 col userhost format A10
 COL client_program_name FORMAT A20 WORD_WRAPPED
 COL error FORMAT A30
@@ -7,13 +10,15 @@ SET VERIFY OFF
 
 ACCEPT vhours NUMBER DEFAULT 1 PROMPT "hours back [1]: " 
       
-SELECT os_username, userhost, client_program_name,
+SELECT dbusername, os_username, userhost, client_program_name,
     TO_CHAR(event_timestamp, 'DD.MM.YYYY HH24:MI') evtime,
     DECODE(return_code, 1017, 'invalid username/password; logon denied',
+           return_code, 28000, 'account locked',
            return_code) error
 FROM sys.UNIFIED_AUDIT_TRAIL
     WHERE action_name='LOGON'
-      AND dbusername='TASGEN'
-      AND return_code<>28000
+      AND (dbusername  IN ('TASGEN', 'ASDM', 'AUTOUSER', 'ZABBIX')
+           OR REGEXP_LIKE(dbusername, '..\(WEB|BAT\).$'))
+      AND return_code<>0
       AND event_timestamp > SYSTIMESTAMP-&vhours./24
       ORDER BY event_timestamp DESC;   
