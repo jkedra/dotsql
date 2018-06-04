5b36674713f96456ef7a0d6a6cc3d946f23cc154
Kedra, Jurek <Jerzy.Kedra@sabre.com>
2018-06-04 14:56:25 +0200

    excluding SYS, DBSNMP

--- sess.sql
+++ sess.sql
@@ -4,10 +4,13 @@ COL machine  FORMAT A10
 COL osuser   FORMAT A10
 col action   FORMAT A20
 
+SET LINES 100
+
 SELECT username, sid||','||serial# sidser,
 	ROUND(seconds_in_wait/60,0) waitmins,
-	machine,osuser,action
+	machine,osuser,action,status
 FROM gv$session
-WHERE username IS NOT NULL
+WHERE NOT (username IS NULL
+           OR username IN ('SYS', 'DBSNMP'))
 ORDER BY username,waitmins;
 
