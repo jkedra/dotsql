834655b14e4d79351b4c402e5b428e96a1de9599
Kedra, Jurek <Jerzy.Kedra@sabre.com>
2018-08-29 10:23:38 +0200

    count for loadmanager

--- count3.sql
+++ count3.sql
@@ -8,19 +8,25 @@ SET ECHO OFF FEEDBACK ON VERIFY OFF TIMING OFF HEAD ON
 
 SET pages 999
 COL count FORMAT 999,999,999
+COL table_name FORMAT A30
+COL owner FORMAT A16
 
 SPOOL &dbname.-count.txt
 SET TIMING ON
 SELECT
-   table_name,
+   owner, table_name,
    TO_NUMBER(
    EXTRACTVALUE(
       XMLTYPE(
          DBMS_XMLGEN.GETXML('SELECT COUNT(*) c FROM '||owner||'.'||table_name))
     ,'/ROWSET/ROW/C')) count
 FROM all_tables
-WHERE OWNER='TASDBA'
-ORDER BY table_name;
+WHERE OWNER IN ('AT', 'ATGUEST', 
+                'ACTIVE_MQ',
+                'COMM',
+                'LPS', 'LPS_ACTIVE', 'LPS_GUEST', 'LPS_SECURITY',
+                'LPS_STATIC', 'LPS_STATS')
+ORDER BY owner, table_name;
 SET TIMING OFF
 
 SPOOL OFF
