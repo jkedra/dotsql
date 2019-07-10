d88bd9548b739c6efb9b79832c0a20707969bfec
Kedra, Jurek <Jerzy.Kedra@sabre.com>
2019-07-10 12:46:43 +0200

    maxutil improved

--- maxutil.sql
+++ maxutil.sql
@@ -1,4 +1,10 @@
+ACCEPT days_to_search DEFAULT 1 PROMPT "days to search [1]: "
+
 SELECT resource_name,max(max_utilization) maxutil,limit_value limit
-FROM DBA_HIST_RESOURCE_LIMIT
+    FROM dba_hist_resource_limit rl, 
+         dba_hist_snapshot s
+    WHERE rl.snap_id = s.snap_id
+      AND rl.dbid = s.dbid
+      AND begin_interval_time >= SYSDATE - &days_to_search
 GROUP BY resource_name,limit_value;
 
